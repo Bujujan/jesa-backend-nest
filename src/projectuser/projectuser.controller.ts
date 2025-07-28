bdec6f63302bea45b3d1e9386a45116c6548469a
Bujujan <bujujan.ru@gmail.com>
2025-07-28 21:44:48 +0100

    can show assigned projects to the logged in user

--- src/projectuser/projectuser.controller.ts
+++ src/projectuser/projectuser.controller.ts
@@ -3,19 +3,43 @@ import { ProjectUserService } from './projectuser.service';
 import { CreateProjectUserDto } from './dto/create-projectuser.dto';
 import { UpdateProjectUserDto } from './dto/update-projectuser.dto';
 import { ProjectUser } from '../models/projectuser.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Controller('projectuser')
 export class ProjectUserController {
-  constructor(private readonly projectUserService: ProjectUserService) {}
+  constructor(private readonly projectUserService: ProjectUserService, @InjectRepository(ProjectUser)
+    private readonly projectUserRepository: Repository<ProjectUser>,) {}
 
   @Get()
   findAll(): Promise<ProjectUser[]> {
     return this.projectUserService.findAll();
   }
 
-  @Get(':uuid')
-  findOne(@Param('uuid') uuid: string): Promise<ProjectUser> {
-    return this.projectUserService.findOne(uuid);
+  @Get(':userId')
+  async getUserProjects(@Param('userId') userId: string) {
+    try {
+      // Find all ProjectUser records for this user and include the project data
+      const projectUsers = await this.projectUserRepository.find({
+        where: { user: { uuid: userId } },
+        relations: ['project'], // This will include the project data
+      });
+
+      // Extract just the projects
+      const projects = projectUsers.map(pu => pu.project);
+
+      return {
+        success: true,
+        projects: projects,
+        count: projects.length
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: 'Failed to fetch user projects',
+        error: error.message
+      };
+    }
   }
 
   @Post()
